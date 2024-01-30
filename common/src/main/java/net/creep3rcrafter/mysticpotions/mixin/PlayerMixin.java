83f57914f226c85cf627b6d6360e9df5e05f6ff9
Brenden Delano <brendendelano@gmail.com>
2024-01-30 16:32:45 -0700

    1.3.0 Publishing

--- common/src/main/java/net/creep3rcrafter/mysticpotions/mixin/PlayerMixin.java
+++ common/src/main/java/net/creep3rcrafter/mysticpotions/mixin/PlayerMixin.java
@@ -1,19 +1,16 @@
 package net.creep3rcrafter.mysticpotions.mixin;
 
 import net.creep3rcrafter.mysticpotions.register.ModEffects;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Player.class)
 public abstract class PlayerMixin extends LivingEntity {
@@ -42,4 +39,13 @@ public abstract class PlayerMixin extends LivingEntity {
         }
     }
      */
+
+    @Inject(method = "attack", at = @At("RETURN"))
+    public void inject2(Entity entity, CallbackInfo ci) {
+        if (this.hasEffect(ModEffects.INFECTION.get())) {
+            if (entity instanceof LivingEntity) {
+                ((LivingEntity) entity).addEffect(new MobEffectInstance(ModEffects.INFECTION.get(), 1200));
+            }
+        }
+    }
 }
