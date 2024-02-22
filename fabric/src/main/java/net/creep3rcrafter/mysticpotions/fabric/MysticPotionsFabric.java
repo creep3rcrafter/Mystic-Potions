516cb088365b81b13c28a14a9b982827a0761a6a
Brenden Delano <brendendelano@gmail.com>
2024-02-21 19:08:16 -0700

    Removed Quilt

--- fabric/src/main/java/net/creep3rcrafter/mysticpotions/fabric/MysticPotionsFabric.java
+++ fabric/src/main/java/net/creep3rcrafter/mysticpotions/fabric/MysticPotionsFabric.java
@@ -1,11 +1,13 @@
 package net.creep3rcrafter.mysticpotions.fabric;
 
-import net.creep3rcrafter.mysticpotions.fabriclike.MysticPotionsFabricLike;
+import net.creep3rcrafter.mysticpotions.BrewingRecipes;
+import net.creep3rcrafter.mysticpotions.MysticPotions;
 import net.fabricmc.api.ModInitializer;
 
 public class MysticPotionsFabric implements ModInitializer {
     @Override
     public void onInitialize() {
-        MysticPotionsFabricLike.init();
+        MysticPotions.init();
+        new BrewingRecipes();
     }
 }
